e81472a2ab1a456a53c9ac6ba91ba6dc6a409f03
Gopal Wagh <54711990+gopalwagh193@users.noreply.github.com>
2020-06-05 00:43:02 -0700

    Add files via upload

---
+++ AddItems.java
@@ -0,0 +1,105 @@
+package com.example.gopal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddItems extends AppCompatActivity implements View.OnClickListener {
+    EditText itemName,itemBrand;
+    Button bAddItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_items);
+
+        itemName = findViewById(R.id.et_item_name);
+        itemBrand = findViewById(R.id.et_brand);
+
+        bAddItem=findViewById(R.id.btn_add_item);
+        bAddItem.setOnClickListener(this);
+    }
+    //This is the part where data is transafeered from Your Android phone to Sheet by using HTTP Rest API calls
+
+    private void   addItemToSheet() {
+
+        final ProgressDialog loading = ProgressDialog.show(this,"Adding Item","Please wait");
+        final String name = itemName.getText().toString().trim();
+        final String brand = itemBrand.getText().toString().trim();
+
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbwuQRfuDl7CHRJJVSXweRdLzmSGTqNXT98L3w2jQz86gadvbBk/exec",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        loading.dismiss();
+                        Toast.makeText(AddItems.this,response,Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> parmas = new HashMap<>();
+
+                //here we pass params
+                parmas.put("action","addItem");
+                parmas.put("itemName",name);
+                parmas.put("brand",brand);
+
+                return parmas;
+            }
+        };
+
+        int socketTimeOut = 50000;// u can change this .. here it is 50 seconds
+
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(retryPolicy);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        queue.add(stringRequest);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v==bAddItem){
+            addItemToSheet();
+
+            //Define what to do when button is clicked
+        }
+    }
+
+
+}
